cafedff1f29a951b9270882398282ab5ea24a45b
Svjat <svjatderev@gmail.com>
2025-05-08 18:09:31 -0400

    Created window with SFML and made sure that BoardState doesn't cause out of bounds errors

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
-
-
 #include "src/BoardState.h"
+#include <SFML/Graphics.hpp>
 
 int main() {
     BoardState bstate;
@@ -8,5 +7,15 @@ int main() {
     bstate.TestViewHalfBoard();
     bstate.PrintBoardVisible();
 
-    return 0;
+    sf::RenderWindow window(sf::VideoMode({800,600}), "Minesweeper");
+    window.setFramerateLimit(30);
+
+    while (window.isOpen()) {
+        while (std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {window.close();}
+
+            window.clear(sf::Color::Green);
+            window.display();
+        }
+    }
 }
