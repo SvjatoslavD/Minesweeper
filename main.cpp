46c664f46ea7786e4830171241454354639a4354
Svjat <svjatderev@gmail.com>
2025-05-09 11:49:03 -0400

    Each board space prints its own value based on if the value is visible or not. Each value is also colored accordingly.

--- main.cpp
+++ main.cpp
@@ -2,19 +2,25 @@
 #include "src/ColorScheme.h"
 #include <SFML/Graphics.hpp>
 
-const sf::Texture createGridTexture(float block_size);
+const sf::Texture CreateGridTexture(float block_size);
 
 int main() {
     BoardState bstate;
+
+    bstate.TestViewBoard();
+    std::cout << bstate.GetMineCount() << " ";
 
     sf::RenderWindow window(sf::VideoMode({850,950}), "Minesweeper");
     window.setFramerateLimit(30);
 
     float block_size = 50.f;
 
-    sf::Texture texture = createGridTexture(block_size);
+    sf::Texture texture = CreateGridTexture(block_size);
+    sf::Texture texture2;
     sf::Sprite sprite(texture);
+    sf::Sprite sprite2(texture);
     sprite.setPosition({25,25});
+    sprite2.setPosition({25,25});
 
     while (window.isOpen()) {
         while (std::optional event = window.pollEvent()) {
@@ -22,12 +28,15 @@ int main() {
 
             window.clear(mono_grey_dark);
             window.draw(sprite);
+            texture2 = bstate.CreateGridTextureValues(block_size);
+            sprite2.setTexture(texture2);
+            window.draw(sprite2);
             window.display();
         }
     }
 }
 
-const sf::Texture createGridTexture(float block_size) {
+const sf::Texture CreateGridTexture(float block_size) {
     sf::RenderTexture grid({800,800});
     grid.clear();
 
--- src/BoardState.h
+++ src/BoardState.h
@@ -7,57 +7,65 @@
 
 #define row 16
 #define col 16
-#define b_size row*col
+#define b_size 256
 
 #include <array>
 #include <iostream>
 #include <random>
+#include <SFML/Graphics.hpp>
+#include "ColorScheme.h"
 
 class BoardState {
 public:
     BoardState() {
+        int max_distrib = 6;
         std::random_device rd;
         std::mt19937 gen(rd());
-        std::uniform_int_distribution distrib(0, 5);
+        std::uniform_int_distribution distrib(0, max_distrib);
 
         for (int i =0; i < row * col; i++) {
             int gen_num = distrib(gen);
-            if (gen_num == 5) {
+            if (gen_num == 0) {
                 board_val[i] = -1;
+                mine_count++;
             }
         }
 
-        std::vector<int> saved_spaces{};
+        while (mine_count < max_mine_count) {
+            max_distrib += 2;
+            std::vector<int> saved_spaces{};
 
-        for (int i =0; i < row * col; i++) {
-            int gen_num = distrib(gen);
-            if (i <= row) {
-                if (board_val[i+row] == -1 || board_val[i+1] == -1) {
-                    if (gen_num == 5) {
-                        saved_spaces.push_back(i);
+            for (int i =0; i < row * col; i++) {
+                int gen_num = distrib(gen);
+                if (i <= row) {
+                    if (board_val[i+row] == -1 || board_val[i+1] == -1) {
+                        if (gen_num == 0) {
+                            saved_spaces.push_back(i);
+                        }
+                    }
+                }
+
+                else if (i >= (b_size-row)) {
+                    if (board_val[i-row] == -1 || board_val[i-1] == -1) {
+                        if (gen_num == 0) {
+                            saved_spaces.push_back(i);
+                        }
+                    }
+                }
+
+                else {
+                    if (board_val[i-row] == -1 || board_val[i+row] == -1 || board_val[i-1] == -1 || board_val[i+1] == -1) {
+                        if (gen_num == 0) {
+                            saved_spaces.push_back(i);
+                        }
                     }
                 }
             }
 
-            else if (i >= (b_size-row)) {
-                if (board_val[i-row] == -1 || board_val[i-1] == -1) {
-                    if (gen_num == 5) {
-                        saved_spaces.push_back(i);
-                    }
-                }
+            for (int num : saved_spaces) {
+                board_val[num] = -1;
+                mine_count++;
             }
-
-            else {
-                if (board_val[i-row] == -1 || board_val[i+row] == -1 || board_val[i-1] == -1 || board_val[i+1] == -1) {
-                    if (gen_num == 5) {
-                        saved_spaces.push_back(i);
-                    }
-                }
-            }
-        }
-
-        for (int num : saved_spaces) {
-            board_val[num] = -1;
         }
 
         for (int i = 0; i < row * col; i++) {
@@ -94,19 +102,56 @@ public:
         }
     }
 
-    void TestViewHalfBoard() {
-        for (int i = b_size/4; i < b_size*3/4; i++) {
+    void TestViewBoard() {
+        for (int i = 0; i < b_size; i++) {
             board_visible[i] = 1;
         }
     }
 
-    static int GetBoardsize() {
-        return b_size;
+    const sf::Texture CreateGridTextureValues(float block_size) {
+        sf::RenderTexture grid({800,800});
+        grid.clear(sf::Color(0,0,0,0));
+
+        sf::Font font = sf::Font("../assets/FiraCode-SemiBold.ttf");
+        sf::Text text = sf::Text(font);
+        text.setCharacterSize(30);
+        text.setScale({1,-1});
+
+        for (int i = 0; i < b_size; i++) {
+            if (board_visible[i] == 1 && board_val[i] != 0) {
+                char val;
+                if (board_val[i] == -1) { val ='M'; }
+                else {val = board_val[i] + '0'; }
+                switch (val) {
+                    case 'M':
+                        text.setFillColor(sf::Color::Black); break;
+                    case  '1':
+                        text.setFillColor(mono_blue); break;
+                    case '2':
+                        text.setFillColor(mono_green); break;
+                    case '3':
+                        text.setFillColor(mono_yellow); break;
+                    case '4':
+                        text.setFillColor(mono_orange); break;
+                    default:
+                        text.setFillColor(mono_red); break;
+                }
+                text.setString(val);
+                text.setPosition({static_cast<float>(i/row)*block_size+15,static_cast<float>(i%row)*block_size+block_size-5});
+                grid.draw(text);
+            }
+        }
+
+        return grid.getTexture();
     }
+
+    int GetMineCount() { return mine_count; }
 
 private:
     std::array<int,b_size> board_val{0};
     std::array<int,b_size> board_visible{0};
+    int mine_count = 0;
+    int max_mine_count = 50;
 };
 
 
